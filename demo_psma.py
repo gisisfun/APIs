29aee506ecb9a3b8932b8d3bf4b38ddb3cb30e82
gisisfun <45181329+gisisfun@users.noreply.github.com>
2018-12-07 14:17:30 +1100

    Update demo_psma.py

--- demo_psma.py
+++ demo_psma.py
@@ -2,7 +2,7 @@ import requests
 import json
 
 
-key = 'brAKupqRdkXNpkYRhy6SpPe1ErVJiAEc'
+key = 'YOUR-KEY'
 full_api_url ='https://api.psma.com.au/beta/v1/addresses?addressString=1%20bowes%20st%20phillip%20act%202606&stateTerritory=ALL&datum=GDA94&page=1&perPage=10'
 
 
